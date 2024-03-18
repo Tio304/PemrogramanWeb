d8adbb10e52b8a53718cb0c9f7d5808d7dd93a88
Tio304 <53498735+Tio304@users.noreply.github.com>
2024-03-18 19:38:07 +0700

    Penyelesaian Modul JQuery Pertemuan Ke-5

---
+++ Pertemuan5/PHP/fungsi.php
@@ -0,0 +1,21 @@
+<?php
+
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+function perkenalan($nama , $salam="Assalamualaikum"){
+    echo $salam." ";
+    echo "Perkenalkan, Nama saya ".$nama."<br>";
+
+
+    echo "Umur saya adalah : ".hitungUmur(2004,2024)."tahun<br/>";
+    echo "Senang Berkenalan dengan Anda <br>";
+}
+
+perkenalan("Kevin");
+
+
+
+
+
