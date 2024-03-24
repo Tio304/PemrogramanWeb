98ec751d526a70756376c4b2b52eb2e3695737b6
Tio304 <53498735+Tio304@users.noreply.github.com>
2024-03-24 12:24:00 +0700

    Penyelesaian Modul PHP  Pertemuan Ke-7

--- Pertemuan7/form_validasi.php
+++ Pertemuan7/form_validasi.php
@@ -2,45 +2,55 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Input FOrm With Validation</title>
+    <title>Input Form With Validation</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body>
-<h1> Form Input Dengan Validasi</h1>
-<form method="post" action="proses_validasi.php">
+<h1>Form Input Dengan Validasi</h1>
+<form method="post" action="proses_validasi.php" id="myForm">
     <label for="nama">Nama : </label>
     <input type="text" id="nama" name="nama">
-    <span id="nama-eror" style="color: red"></span><br>
-
-    <br>
+    <span id="nama-error" style="color: red"></span><br><br>
 
     <label for="email">Email : </label>
-    <input  type="text" id="email" name="email">
-    <span id="email-eror" style="color: red"></span><br>
-    <br>
+    <input type="text" id="email" name="email">
+    <span id="email-error" style="color: red"></span><br><br>
+
+    <label for="password">Password : </label>
+    <input type="text" id="password" name="password">
+    <span id="password-error" style="color: red"></span><br><br>
 
     <input type="submit" value="submit">
 </form>
 <script>
     $(document).ready(function (){
-        $("#myForm").submit(function (event){
-            var nama = $("#nama").val;
-            var email = $("#email").val;
+        $("form").submit(function (event){
+            var nama = $("#nama").val();
+            var email = $("#email").val();
+            var password = $("#password").val();
             var valid = true;
 
             if(nama === ""){
-                $("#nama-eror").text("Nama Harus Di isi");
+                $("#nama-error").text("Nama Harus Di isi");
                 valid=false;
             } else{
-                $("#nama-eror").text(" ");
+                $("#nama-error").text("");
             }
-            if(nama === ""){
-                $("#email-eror").text("Email Harus Di isi");
+            if(email === ""){
+                $("#email-error").text("Email Harus Di isi");
                 valid=false;
             } else{
-                $("#email-eror").text(" ");
+                $("#email-error").text("");
             }
 
-            if(valid){
+            if(password === ""){
+                $("#password-error").text("Password Harus Di isi");
+                valid=false;
+            } else{
+                $("#password-error").text("");
+            }
+
+            if(!valid){
                 event.preventDefault();
             }
         })
--- Pertemuan7/proses_validasi.php
+++ Pertemuan7/proses_validasi.php
@@ -1,1 +1,22 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $nama = $_POST["nama"];
+    $email = $_POST["email"];
+    $errors = array();
+
+    if(empty($nama)){
+        $errors[] = "Nama Harus diisi.";
+    }
+
+    if(empty($email)){
+       $errors[] = "Email harus diisi";
+    } elseif (filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $errors[] = "Format Tidak Valid.";
+    }
+
+    if(!empty($errors)){
+        foreach ($errors as $eror){
+            echo $eror."<br>";
+        }
+    }
+}
